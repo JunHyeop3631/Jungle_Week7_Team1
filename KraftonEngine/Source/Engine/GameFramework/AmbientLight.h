608046bc9f51fade51cccaa3f2ea59f2cafdbd96
keonwookang0914 <kwkang0914@gmail.com>
2026-04-18 20:32:50 +0900

    feat: Ambient, DirectionalActor 생성

---
+++ KraftonEngine/Source/Engine/GameFramework/AmbientLight.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "AActor.h"
+
+class UAmbientLightComponent;
+class UBillboardComponent;
+
+class AAmbientLight : public AActor
+{
+public:
+	AAmbientLight();
+
+private:
+	UAmbientLightComponent* AmbientLight;
+	UBillboardComponent* SpriteComponent;
+};
+
---
+++ KraftonEngine/Source/Engine/GameFramework/DirectionalLight.cpp
@@ -0,0 +1,14 @@
+﻿#include "DirectionalLight.h"
+#include "Components/DirectionalLightComponent.h"
+#include "Components/BillboardComponent.h"
+
+ADirectionalLight::ADirectionalLight()
+{
+	DirectionalLight = AddComponent<UDirectionalLightComponent>();
+	SetRootComponent(DirectionalLight);
+
+	SpriteComponent = AddComponent<UBillboardComponent>();
+	SpriteComponent->AttachToComponent(DirectionalLight);
+	SpriteComponent->SetTexture(FName("DirectionalLightIcon"));
+	DirectionalLight->SetEditorIconBillboard(SpriteComponent);
+}
